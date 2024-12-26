4c18a3f239d7199e84689020ff21251c41b88549
DAMIANG97 <damiangrabowski975@gmail.com>
2024-12-26 01:29:48 +0100

    Add data file
    - Add data file
    - Render links from data

--- components/Organisms/Footer/index.tsx
+++ components/Organisms/Footer/index.tsx
@@ -2,28 +2,17 @@ import React from "react";
 import styles from "./Footer.module.scss";
 import LogoLink from "../../Atoms/LogoLink";
 import LinkSection from "@/components/Molecules/LinkSection";
+import { logoLinks, links } from "@/public/data/links";
 
 const Footer = () => {
-  const links = [
-    {
-      href: "/CV_Damian_Grabowski_.pdf",
-      children: "Download CV",
-      download: "CV_Damian_Grabowski_.pdf",
-    },
-    {
-      href: "mailto:damiangrabowski975@gmail.com",
-      children: "Contact Me",
-    },
-  ];
-
   return (
     <footer className={styles.footer}>
       <LinkSection links={links} />
 
-      <div className={styles.prawo}>
-        <LogoLink href="https://github.com/DAMIANG97/" src="/logo/github.svg" alt="GitHub" />
-        <LogoLink href="https://linkedin.com/in/damiang97/" src="/logo/linkedIn_whiteV.svg" alt="LinkedIn" />
-        <LogoLink href="https://damiang97.itch.io/" src="/logo/itchio.svg" alt="itch.io" width={128} />
+      <div className={styles.right}>
+        {logoLinks.map((logo, index) => (
+          <LogoLink key={index} href={logo.href} src={logo.src} alt={logo.alt} width={logo.width}></LogoLink>
+        ))}
       </div>
     </footer>
   );
---
+++ public/data/links.ts
@@ -0,0 +1,30 @@
+export const links = [
+  {
+    href: "/CV_Damian_Grabowski_.pdf",
+    children: "Download CV",
+    download: "CV_Damian_Grabowski_.pdf",
+  },
+  {
+    href: "mailto:damiangrabowski975@gmail.com",
+    children: "Contact Me",
+  },
+];
+
+export const logoLinks = [
+  {
+    href: "https://github.com/DAMIANG97/",
+    src: "/logo/github.svg",
+    alt: "GitHub",
+  },
+  {
+    href: "https://linkedin.com/in/damiang97/",
+    src: "/logo/linkedIn_whiteV.svg",
+    alt: "LinkedIn",
+  },
+  {
+    href: "https://damiang97.itch.io/",
+    src: "/logo/itchio.svg",
+    alt: "itch.io",
+    width: 128,
+  },
+];

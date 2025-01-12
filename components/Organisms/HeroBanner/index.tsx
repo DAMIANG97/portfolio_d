f74fb05b347b8549340f4960b63a0fec70126847
DAMIANG97 <damiangrabowski975@gmail.com>
2025-01-13 00:51:37 +0100

    Update headar and hero banner
    -Adding dynamic buttons and background for updated components.

--- components/Organisms/HeroBanner/index.tsx
+++ components/Organisms/HeroBanner/index.tsx
@@ -5,18 +5,19 @@ import clsx from "clsx";
 import Image from "next/image";
 import Greetings from "@/components/Molecules/Greetings";
 import { useGlobalContext } from "../../../context/GlobalContext";
+import { careers } from "@/data/careers";
 
 const HeroBanner: React.FC = () => {
   const { activeCareer } = useGlobalContext();
 
+  const activeCareerData = careers.find((career) => career.id === activeCareer);
+
   const heroClass = clsx(styles["hero-banner"], {
-    [styles["hero-banner--game-dev"]]: activeCareer === "Game Development",
-    [styles["hero-banner--pixel-art"]]: activeCareer === "Pixel Art",
-    [styles["hero-banner--electrical-design"]]: activeCareer === "Electrical Designer",
+    [styles[`hero-banner--${activeCareerData?.id}`]]: activeCareerData,
   });
 
   return (
-    <section className={heroClass}>
+    <section className={heroClass} aria-label={activeCareerData?.description || "Hero Banner"}>
       <Greetings />
       <Image className={styles["hero-banner__image2"]} alt="Photo of Me" src="/hero/My_Photo.png" width={2000} height={2000} />
     </section>

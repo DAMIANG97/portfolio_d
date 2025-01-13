fdce354df083b67f974b2817912898171b850744
DAMIANG97 <damiangrabowski975@gmail.com>
2025-01-14 00:44:46 +0100

    Add MainComponent

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroBanner from "../components/Organisms/HeroBanner/index";
 import Header from "../components/Organisms/Header/index";
 import Footer from "../components/Organisms/Footer/index";
+import MainContent from "../components/Organisms/MainContent/index";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroBanner />
+      <MainContent></MainContent>
       <Footer></Footer>
     </div>
   );
---
+++ components/Organisms/MainContent/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./MainContent.module.scss";
+
+const MainContent: React.FC = () => {
+  return <main className={styles.main}></main>;
+};
+
+export default MainContent;

b3c4facde5bf04849d165c85a86423dba3b9b1bc
DAMIANG97 <damiangrabowski975@gmail.com>
2025-01-27 01:05:46 +0100

    Update menu component
    - Update hamburger menu

--- components/Atoms/LogoLink/index.tsx
+++ components/Atoms/LogoLink/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./LogoLink.module.scss";
 import Image from "next/image";
+import clsx from "clsx";
 
 interface LogoLinkProps {
   ariaLabel: string;
@@ -13,9 +14,9 @@ interface LogoLinkProps {
   className?: string;
 }
 
-const LogoLink: React.FC<LogoLinkProps> = ({ href = "#", src = "/logo/github.svg", alt = "Github logo", width = 32, height = 32, ariaLabel, title }) => {
+const LogoLink: React.FC<LogoLinkProps> = ({ href = "#", src = "/logo/github.svg", alt = "Github logo", width = 32, height = 32, ariaLabel, title, className }) => {
   return (
-    <a title={title} aria-label={ariaLabel} className={styles["logo-link"]} href={href} target="_blank" rel="noopener noreferrer">
+    <a title={title} aria-label={ariaLabel} className={clsx(styles["logo-link"], styles[`${className}`])} href={href} target="_blank" rel="noopener noreferrer">
       <Image src={src} alt={alt} width={width} height={height} />
     </a>
   );
--- components/Organisms/Header/index.tsx
+++ components/Organisms/Header/index.tsx
@@ -7,7 +7,7 @@ import LogoLink from "@/components/Atoms/LogoLink";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <LogoLink href="#" alt="LOGO" ariaLabel="A man sitting at a computer and programming. With the title - Damian Grabowski" src="/logo/logo.png" height={50} width={266}></LogoLink>
+      <LogoLink className="logo-link--header" href="#" alt="LOGO" ariaLabel="A man sitting at a computer and programming. With the title - Damian Grabowski" src="/logo/logo.png" height={50} width={266}></LogoLink>
       <Navigation></Navigation>
     </header>
   );

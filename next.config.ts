f0352ee78ea9cbbdb04f92c6edd7b1ae595a5986
DAMIANG97 <damiangrabowski975@gmail.com>
2025-01-28 21:55:22 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Konfiguracja dla statycznego eksportu
+  trailingSlash: true, // Używaj '/' na końcu ścieżek (wymagane dla GitHub Pages)
+  basePath: "/<portfolio_d>", // Zamień <nazwa-repozytorium> na nazwę swojego repo
 };
 
-export default nextConfig;
+module.exports = nextConfig;

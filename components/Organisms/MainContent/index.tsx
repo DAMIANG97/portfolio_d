8c51cb862446df6a1d99ff6dae687c09f0c7c5bb
DAMIANG97 <damiangrabowski975@gmail.com>
2025-02-17 00:36:33 +0100

    Fullfill MainContent component, minor style update

--- components/Organisms/MainContent/index.tsx
+++ components/Organisms/MainContent/index.tsx
@@ -1,7 +1,38 @@
+import Wrapper from "@/components/Atoms/Wrapper";
 import styles from "./MainContent.module.scss";
 
 const MainContent: React.FC = () => {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Wrapper className="wrapper--main-content">
+        <section className={styles["main__professional-summary"]}></section>
+        <section className={styles["main__exp-section"]}>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+        </section>
+        <section className={styles["main__exp-section"]}>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+          <p>z</p>
+        </section>
+      </Wrapper>
+    </main>
+  );
 };
 
 export default MainContent;
